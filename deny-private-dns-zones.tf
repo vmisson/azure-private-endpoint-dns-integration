35b2d1655a7b3235c10ac05dd6e5facc631a6dec
Vincent Misson <git@misson.cloud>
2023-06-02 11:25:28 +0200

    Update variables

--- deny-private-dns-zones.tf
+++ deny-private-dns-zones.tf
@@ -1,11 +1,13 @@
 resource "random_id" "deny_private_dns_zones_id" {
+  count = var.deny_prive_dns_zone_creation ? 1 : 0
   byte_length = 16
 }
 
 resource "azurerm_management_group_policy_assignment" "deny_private_dns_zones_assignment" {
-  name                 = random_id.deny_private_dns_zones_id.id
+  count = var.deny_prive_dns_zone_creation ? 1 : 0
+  name                 = random_id.deny_private_dns_zones_id[0].id
   management_group_id  = data.azurerm_management_group.root.id
-  policy_definition_id = azurerm_policy_definition.deny_policy.id
+  policy_definition_id = azurerm_policy_definition.deny_policy[0].id
   display_name         = "Deny Private DNS Zones outside of the DNS Resource Group"
   non_compliance_message {
     content = "Private DNS Zones are not allowed outside of the DNS Resource Group"
@@ -15,6 +17,7 @@ resource "azurerm_management_group_policy_assignment" "deny_private_dns_zones_as
 }
 
 resource "azurerm_policy_definition" "deny_policy" {
+  count = var.deny_prive_dns_zone_creation ? 1 : 0
   name         = "DenyPrivateDNSZones"
   policy_type  = "Custom"
   mode         = "Indexed"
--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
 variable "location" {
+  type = string
   default = "westeurope"
 }
 
 variable "resource_group_dns_name" {
+  type = string
   default = "private-dns-zones-rg"
 }
 
 variable "user_assigned_identity_name" {
+  type = string
   default = "dns-remediation-managed-identity"
+}
+
+variable "deny_prive_dns_zone_creation" {
+  type = bool
+  default = true
 }
