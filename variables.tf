a3970c9e33990dee81c1f2b2688d913bd68656ab
Vincent Misson <git@misson.cloud>
2025-08-28 10:30:43 +0200

    Update private-zones for AI Foundry and minor updates on Terraform code.

--- variables.tf
+++ variables.tf
@@ -1,16 +1,23 @@
+variable "subscription_id" {
+  description = "The ID of the Azure subscription where the resources will be created."
+  type        = string
+}
+
 variable "location" {
+  description = "The Azure region where the resources will be created."
   type = string
   default = "westeurope"
 }
 
 variable "resource_group_dns_name" {
+  description = "Name of the resource group"
   type = string
-  default = "private-dns-zones-rg"
+  default = "rg-dns-priv-001"
 }
 
 variable "user_assigned_identity_name" {
   type = string
-  default = "dns-remediation-managed-identity"
+  default = "mi-dns-remediation"
 }
 
 variable "deny_prive_dns_zone_creation" {
